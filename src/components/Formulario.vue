851a8b6fdf49e954cdd671278b8e14c4b8df39e3
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2022-10-26 12:52:02 -0300

    #FEAT: Transformando o formulário das tarefas em composition API

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -15,20 +15,17 @@
                 </div>
             </div>
             <div class="column">
-                <Temporizador @aoFinalizarTemporizador="finalizarTarefa"/>
+                <Temporizador @aoFinalizarTemporizador="salvarTarefa"/>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { TipoNotificacao } from '@/interfaces/INotificacao'
 import { key } from '@/store'
-import { ADICIONAR_TAREFA } from '@/store/tipo-mutacoes'
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import { useStore } from 'vuex'
 import Temporizador from './Temporizador.vue'
-import useNotificador from '@/hooks/notificador'
 import { CADASTRAR_TAREFA } from '@/store/tipo-acoes'
 
 export default defineComponent({
@@ -36,37 +33,28 @@ export default defineComponent({
     components: {
         Temporizador
     },
-    data() {
-        return {
-            descricao: '',
-            idProjeto: ''
-        }
-    },
-    methods: {
-        finalizarTarefa(tempoEmSegundos: number) : void {
-            const projeto = this.projetos.find(proj => proj.id === this.idProjeto)
-
-            if (!projeto) {
-                this.notificar(TipoNotificacao.FALHA, 'Ops!', 'Selecione um projeto antes de finalizar a tarefa.')
-                return
-            }
-
-            this.store.dispatch(CADASTRAR_TAREFA, {
-                duracaoEmSegundos: tempoEmSegundos,
-                descricao: this.descricao,
-                projeto: projeto
-            })
-            this.descricao = ''
-        }
-    },
     setup() {
-        const store = useStore(key) 
-        const { notificar } = useNotificador()
+        const store = useStore(key)
+
+        const descricao = ref('')
+        const idProjeto = ref('')
+        const projetos = computed(() => store.state.projeto.projetos)
+
+        const salvarTarefa = (tempoEmSegundos: number) : void => {
+            store.dispatch(CADASTRAR_TAREFA, {
+                duracaoEmSegundos: tempoEmSegundos,
+                descricao: descricao.value,
+                projeto: projetos.value.find(proj => proj.id === idProjeto.value)
+            })
+            descricao.value = ''
+        }
 
         return {
             store,
-            projetos: computed(() => store.state.projeto.projetos),
-            notificar
+            descricao,
+            idProjeto,
+            projetos,
+            salvarTarefa
         }
     }
 })
